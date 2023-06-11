6427c758c8ca5f460c06b684ebfd5772bb9fb993
zhyunk <gimwlgus@gmail.com>
2023-06-12 00:36:35 +0900

    QueryDSL Config 파일 작성
    JPAQueryFactory 객체를 
    
    Bean 객체로 등록

---
+++ jpaStudy/src/main/java/com/springboot/jpa/config/QueryDSLConfiguration.java
@@ -0,0 +1,20 @@
+package com.springboot.jpa.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+public class QueryDSLConfiguration {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
