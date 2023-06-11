fe3358015876e8fe90b6d1b72d0a88b3b6ac95ec
zhyunk <gimwlgus@gmail.com>
2023-06-11 22:30:54 +0900

    ＠Query 사용 1
    ?1 : where 절의 첫 번째 파라미터와 매개변수를 매핑

--- jpaStudy/src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
+++ jpaStudy/src/main/java/com/springboot/jpa/data/repository/ProductRepository.java
@@ -3,10 +3,14 @@ package com.springboot.jpa.data.repository;
 import com.springboot.jpa.data.entity.Product;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByName(String name, Sort sort);
+
+    @Query("SELECT p FROM Product AS p WHERE p.name = ?1")
+    List<Product> findByName(String name);
 }
