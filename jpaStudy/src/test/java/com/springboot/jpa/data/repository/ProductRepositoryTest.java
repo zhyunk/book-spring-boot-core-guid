9d2a4c61b34d0ff9ddc64ff06c1f1790d32b31d4
zhyunk <gimwlgus@gmail.com>
2023-06-12 00:00:49 +0900

    QueryDSL 테스트 1 : JPAQuery 사용

--- jpaStudy/src/test/java/com/springboot/jpa/data/repository/ProductRepositoryTest.java
+++ jpaStudy/src/test/java/com/springboot/jpa/data/repository/ProductRepositoryTest.java
@@ -1,13 +1,19 @@
 package com.springboot.jpa.data.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import com.springboot.jpa.data.entity.Product;
+import com.springboot.jpa.data.entity.QProduct;
+import net.minidev.json.JSONUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.Order;
 import org.springframework.data.domain.Sort;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +22,43 @@ class ProductRepositoryTest {
 
     @Autowired
     ProductRepository productRepository;
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Test
+    void queryDslTest() {
+        // QueryDSL을 사용하기 위해 JPAQuery 객체 사용
+        // JPAQuery 객체는 entityManager를 활용해 생성한다.
+        JPAQuery<Product> query = new JPAQuery<>(entityManager);
+        QProduct qProduct = QProduct.product;
+
+        List<Product> productList = query
+                .from(qProduct)
+                .where(qProduct.name.eq("펜"))
+                .orderBy(qProduct.price.asc())
+                .fetch(); // list 타입으로 반환받기 위해 fetch() 사용
+             /*   조회 결과를 반환 받기 위한 메서드
+                  fetch() : 리스트로 반환
+                  fetchOne : 단 건의 조회 결과 반환
+                  fetchFirst() : 여러 건의 조회 결과 중 1건 반환
+                  fetchCount() : 조회 결과의 개수 반환
+                  fetchResults() : 조회 결과 리스트와 개수를 포함한 QueryResults를 반환  */
+
+        System.out.println("=============================================== size : " + productList.size());
+
+        for (Product product: productList) {
+            System.out.println("==========================================");
+            System.out.println();
+            System.out.println("product number = " + product.getNumber());
+            System.out.println("product name = " + product.getName());
+            System.out.println("product price = " + product.getPrice());
+            System.out.println("product stock = " + product.getStock());
+            System.out.println();
+            System.out.println("==========================================");
+        }
+
+    }
 
     @Test
     void sortingAndPagingTest() {
