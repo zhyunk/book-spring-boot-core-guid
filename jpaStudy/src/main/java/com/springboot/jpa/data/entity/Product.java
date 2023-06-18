0cc1a654c4d1864d75870fcf722c8e8fc57d5c49
zhyunk <gimwlgus@gmail.com>
2023-06-18 19:48:18 +0900

    JPA 연관관계 : 1대1 양방향 실습

--- jpaStudy/src/main/java/com/springboot/jpa/data/entity/Product.java
+++ jpaStudy/src/main/java/com/springboot/jpa/data/entity/Product.java
@@ -29,4 +29,7 @@ public class Product extends BaseEntity {
     @Column(nullable = false)
     private Integer stock;
 
+    @OneToOne(mappedBy = "product")
+    @ToString.Exclude
+    private ProductDetail productDetail;
 }
