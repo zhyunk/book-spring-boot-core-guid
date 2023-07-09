e2bf391ac9de752801bb91844c305330da42e12c
zhyunk <gimwlgus@gmail.com>
2023-07-10 02:03:54 +0900

    시큐리티 & JWT : JwtTokenProvider 구현
    책에서 예제로 사용된 
    Jwts의 signWith() 메서드의 오버로딩 구현 몇 개와 
    parser() 메서드가 deprecated 되어
    책에서 나온 방법과는 조금 다르게 구현하였다.
    
    ---
    
    # 1. 
    Jwts.builder().setWith(secretKey , algoritm) 
    
     위 내용을 다음과 같이 작성.
    
    Jwts.builder().setWith(key) 
    
    
    secretKey 사용하는 방법이 
    deprecated 되어 못쓰게 되었기 때문에,
    
    Key를 생성한 후 , ( Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256); ) 
    Key를 setWith()에 인자로 넣어주는 방법을 사용함
    
    ---
    
    # 2. 
    Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
    
      위 내용에서 parser() 메서드가 deprecated 되어 다음과 같이 작성.
    
    Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token)

---
+++ jpaStudy/src/main/java/com/springboot/jpa/config/security/JwtTokenProvider.java
@@ -0,0 +1,94 @@
+package com.springboot.jpa.config.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtTokenProvider {
+
+    private final UserDetailsService service;
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private final long tokenValidMillisecond = 1000L * 60 * 60;
+
+    public String createToken(String userUid, List<String> roles) {
+        log.info("[createToken] 토큰 생성 시작");
+        
+        Claims claims = Jwts.claims().setSubject(userUid);
+        claims.put("roles", roles); // 토큰을 사용하는 사용자 권한 확인을 위해 입력
+        Date now = new Date();
+
+        // 토큰 생성
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + tokenValidMillisecond))
+                .signWith(key)
+                .compact();
+
+        log.info("[createToken] 토큰 생성 완료");
+        return token;
+    }
+
+    // 필터에서 인증이 성공했을 때 SecurityContextHolder에 저장할 Authentication을 생성
+    public Authentication getAuthentication(String token) {
+        log.info("[getAuthentication] 토큰 인증 정보 조회 시작");
+
+        UserDetails userDetails = service.loadUserByUsername(this.getUsername(token));
+
+        log.info("[getAuthentication] 토큰 인증 정보 조회 완료, UserDetails username : {}", userDetails.getUsername());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token) {
+        log.info("[getUsername] 토큰 기반 회원 구별 정보 추출");
+        String info = Jwts
+                .parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+        log.info("[getUsername] 토큰 기반 회원 구별 정보 추출 완료, info : {}", info);
+        return info;
+    }
+    
+    public String resolveToken(HttpServletRequest request) {
+        log.info("[resolveToken] HTTP 헤더에서 Token 값 추출");
+
+        return request.getHeader("X-AUTH-TOKEN");
+    }
+
+    public boolean validateToken(String token) {
+        log.info("[validateToken] 토큰 유효 체크 시작");
+
+        try {
+            Jws<Claims> claims = Jwts
+                    .parserBuilder()
+                    .setSigningKey(key).build()
+                    .parseClaimsJws(token);
+
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch(Exception e) {
+            log.info("[validateToken] 토큰 유효 체크 예외 발생");
+
+            return false;
+        }
+    }
+}
