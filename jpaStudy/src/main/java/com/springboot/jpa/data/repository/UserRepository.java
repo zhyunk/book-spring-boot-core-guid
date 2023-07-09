dd9550ac5b88fae43f4346f38776b7aa4909f1f4
zhyunk <gimwlgus@gmail.com>
2023-07-10 01:15:01 +0900

    시큐리티 & JWT : `UserDetailsService`를 상속받아 Service 를 작성하고, Repository 구현
    Service에서 사용하기 위해
    
    Repository를 구현하였고,
    
    Repository 에서 사용할 메서드로는
    
    데이터베이스에서 username (사용자 아이디) 를 확인하고 
    
    존재하는 아이디인 경우 
    
    해당 아이디 관련 정보를 
    
    User 객체로 반환하는 메서드를 작성하였다.

---
+++ jpaStudy/src/main/java/com/springboot/jpa/data/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springboot.jpa.data.repository;
+
+import com.springboot.jpa.data.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User getByUid(String uid);
+
+}
---
+++ jpaStudy/src/main/java/com/springboot/jpa/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springboot.jpa.service.impl;
+
+import com.springboot.jpa.data.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("[loadByUsername] loadUserByUsername 수행 . username = {} ", username);
+        return userRepository.getByUid(username);
+    }
+
+}
