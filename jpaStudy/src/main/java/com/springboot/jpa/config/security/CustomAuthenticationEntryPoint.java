6d85b54f9d51433f9fd925dc3b6a27f3c93e3599
zhyunk <gimwlgus@gmail.com>
2023-07-10 02:51:53 +0900

    시큐리티 & JWT : CustomAuthenticationEntryPoint 구현
    인증이 실패한 상황을 처리하는 
    
    `AuthenticationEntryPoint` 인터페이스를 구현한 
    
    `CustomAuthenticationEntryPoint` 를 작성하였다.
    
    CustomAuthenticationEntryPoint 에서 
    EntryPointErrorResponse 라는 dto 클래스 사용.

---
+++ jpaStudy/src/main/java/com/springboot/jpa/config/security/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package com.springboot.jpa.config.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.jpa.data.dto.EntryPointErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ObjectMapper mapper = new ObjectMapper();
+        log.info("[commence] 인증 실패로 response.sendError 발생");
+
+        EntryPointErrorResponse entryPointErrorResponse = new EntryPointErrorResponse();
+        entryPointErrorResponse.setMsg("인증이 실패하였습니다.");
+
+        response.setStatus(401);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write(mapper.writeValueAsString(entryPointErrorResponse));
+    }
+
+}
